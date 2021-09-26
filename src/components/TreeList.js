5071c390dde64f539b8c7c03c192ec408aaff0a3
Botond Gorfol <botond.gorfol@gmail.com>
2021-09-26 19:29:46 +0200

    Refactor: divide TreesPage into a smart and a dumb component

---
+++ src/components/TreeList.js
@@ -0,0 +1,43 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function TreeList(props) {
+    function renderRow(tree) {
+        return (
+            <tr key={tree.id}>
+                <td>{tree.species}</td>
+                <td>{tree.ownerId}</td>
+                <td>{tree.family}</td>
+            </tr>
+        );
+    };
+
+    return (
+        <>
+            <h2>Trees</h2>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th>Species</th>
+                        <th>Owner ID</th>
+                        <th>Family</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {props.trees.map(renderRow)}
+                </tbody>
+            </table>
+        </>
+    );
+}
+
+TreeList.propTypes = {
+    trees: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        species: PropTypes.string.isRequired,
+        ownerId: PropTypes.number.isRequired,
+        family: PropTypes.string.isRequired
+    })).isRequired
+}
+
+export default TreeList;
--- src/components/TreesPage.js
+++ src/components/TreesPage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { getTrees } from "../api/treeApi";
+import TreeList from "./TreeList";
 
 function TreesPage() {
     const [trees, setTrees] = useState([]);
@@ -8,31 +9,10 @@ function TreesPage() {
         getTrees().then(_trees => setTrees(_trees));
     }, []);
 
-    function renderRow(tree) {
-        return (
-            <tr key={tree.id}>
-                <td>{tree.species}</td>
-                <td>{tree.ownerId}</td>
-                <td>{tree.family}</td>
-            </tr>
-        );
-    };
-
     return (
         <>
             <h2>Trees</h2>
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th>Species</th>
-                        <th>Owner ID</th>
-                        <th>Family</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {trees.map(renderRow)}
-                </tbody>
-            </table>
+            <TreeList trees={trees} />
         </>
     );
 }
